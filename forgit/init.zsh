4f22f9a621c15673cb207714b2c86eb65a15c4ee
jade <jadearmy89@gmail.com>
2020-06-02 18:39:27 +0700

    Update forgit

---
+++ forgit/init.zsh
@@ -0,0 +1,23 @@
+
+forgit_log=gzl
+forgit_diff=gzd
+forgit_add=gza
+forgit_reset_head=gzr
+forgit_ignore=gzi
+forgit_restore=gzf
+forgit_clean=gzc
+forgit_stash_show=gzs
+
+# Enter	Confirm
+# Tab	Toggle mark
+# ?	Toggle preview window
+# Alt - W	Toggle preview wrap
+# Ctrl - S	Toggle sort
+# Ctrl - R	Toggle selection
+# Ctrl - K / P	Selection move up
+# Ctrl - J / N	Selection move down
+# Alt - K / P	Preview move up
+# Alt - J / N	Preview move down
+
+# Source module files.
+source "${0:h}/external/forgit.plugin.zsh" || return 1
