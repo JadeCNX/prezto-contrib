e3066b74f4a9b69265edc60b690d61c992d6c47b
jade <jadearmy89@gmail.com>
2020-06-09 18:13:29 +0700

    Add git alias glm, glM

--- git-contrib/alias.zsh
+++ git-contrib/alias.zsh
@@ -193,11 +193,13 @@ if ! zstyle -t ':prezto:module:git:alias' skip 'yes'; then
   alias gld='git log --topo-order --stat --patch --full-diff --pretty=format:"${_git_log_medium_format}"'
   alias glD='git log --topo-order --stat --patch --word-diff --full-diff --pretty=format:"${_git_log_medium_format}"'
   alias glo='git log --topo-order --pretty=format:"${_git_log_oneline_with_author_format}"'
-  alias glg='git log --topo-order --all --graph --pretty=format:"${_git_log_oneline_format}"'
+  alias glg='git log --topo-order --all --graph --pretty=format:"${_git_log_oneline_with_author_format}"'
   alias glb='git log --topo-order --pretty=format:"${_git_log_brief_format}"'
   alias glc='git shortlog --summary --numbered'
   alias glS='git log --show-signature'
   alias glp='git log --patch'
+  alias glm='git log --topo-order --oneline --date=relative --author=`git config user.name` --pretty=format:"${_git_log_oneline_format}"'
+  alias glM='git log --topo-order --oneline --stat --date=relative --author=`git config user.name` --pretty=format:"${_git_log_oneline_format}"'
 
   # Merge (m)
   alias gm='git merge'
